0e0ac8d159420563db4ebd74acee43b478616d3c
unknown <cdquintero7@gmail.com>
2020-07-07 22:16:00 -0700

    updated appointment scheduling program

--- AppointmentMaker/appointment.c
+++ AppointmentMaker/appointment.c
@@ -1,5 +1,5 @@
 // Christian Quintero
-// COEN 10L 
+// COEN10L 
 // FILE: appointment.c
 //
 // This program simulates an appointment scheduling system, giving the user the option
@@ -7,67 +7,182 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+int i;
+int counter=0;
+char names[6][20];
 
-int appointment[6];//Array that will contain 6 integers.
+void check()//function to check for letter
+{
+	int lcounter=0;
+	char *l;
+	char letter;
+	printf("please enter the letter that you want to count.\n");
+	scanf (" %c", &letter);//letter that will be checked
+	for(i=0;i<6;i++)//go through entire array
+	{
+		l=names[i];//set the pointer
+		while(*l!='\0')
+		{
+			if(*l==letter)
+			{
+				lcounter++;
+			}
+			l++;
+		}
+	}
+	printf("The letter %c appeared %d times\n", letter, lcounter++);//prints number of occurrences
+	return;	
+}
+
+void emergency()//function to make emergenecy appointment
+{
+	int k;
+	char emerg[20];
+	printf("Please enter the name of the individual that has an emergency.\n");
+	scanf("%s",emerg);//name for emergency
+	for (i=0;i<6;i++)//to check the array
+	{
+		if (strcmp(names[i],emerg)==0)//check for repeating names
+		{
+			printf("This name is already taken. Please use another name next time.\n");
+			return;
+		}
+	}
+	if (counter==6)//If all slots are taken up
+	{
+		printf("To make this appointment we will have to cancel someone else's appointment.\n");
+		names[6][0]='\0';//last string in array gets erased to make room for emergency	
+	}
+	for (k=5;k>0;k--)//move strings down in array
+	{
+		strcpy(names[k],names[k-1]);
+	}
+	printf("Your emergency appointment has been scheduled for 1p.m.\n");
+	strcpy(names[0],emerg);//set emergency as first appointment
+	if (counter!=6)
+	{
+		counter++;
+	}
+	return;
+}
+void make ()//Fuction for scheduling a new appointment
+{
+        char enter[20];
+        if (counter==6)//This checks to see if slots are full
+        {
+                printf("there are currently no available appointments.\n");
+        }
+        else//Will enter name into appointment
+        {
+                printf("Please enter your name to schedule an appointment.\n");
+                scanf("%s",enter);
+                for (i=0;i<6;i++)//To check for repeating names
+                {
+                        if (strcmp(names[i],enter)==0)//If name entered already exists
+                        {
+                                printf("Sorry that name is already taken. Please use another name next time.\n");
+                                break;
+                        }
+                }
+                if (i==6)//If there are no repeating names
+                {
+                        printf("Your appointment will be at %d pm\n", counter+1);
+                        strcpy(names[counter],enter);
+                        counter++;
+                }
+        }
+        return;
+}
+
+void delete()//Function for canceling an appointment
+{
+        char cancel[20];
+        int j;
+        if (counter==0)//No appointments 
+        {
+                printf("There are no scheduled appointments.\n");
+        }
+        else//Cancel appointment
+        {
+                printf("To cancel your appointment please enter your name\n");
+                scanf("%s",cancel);
+                for (i=0;i<6;i++)//To go through array looking for name
+                {
+
+                        if (strcmp(names[i],cancel)==0)//if name matches
+                        {
+                                if (i==5)//To empty last name in array
+                                {
+                                        names[i][0]='\0';
+                                }
+                                printf("Your appointment has been cancelled.\n");
+                                for (j=i+1;j<6;j++)//To move all other appointments down
+                                {
+                                        strcpy(names[j-1],names[j]);
+                                        names[j][0]='\0';
+                                }
+                                counter--;
+                                break;
+                        }
+                }
+                if (i==6)//did not find name
+                {
+                        printf("There is no appointment scheduled under that name.\n");
+                }
+        }
+        return;
+}
+
+void view()//Function for viewing all appointments currently scheduled
+{
+        if (counter==0)//no appointments scheduled
+        {
+                printf("There are no appointments scheduled\n");
+        }
+        else
+        {
+                for (i=0;i<6;i++)//To show all scheduled appointments in array
+                {
+                        printf("Scheduled appointments at %d p.m.   :%s \n",i+1,names[i]);
+                }
+        }
+        return;
+}
 
 int main()
 {
-	int i;
-	int counter=0;
-	int action;
-	int appnum;
-	int ID;
-	for (i=0; i<6; i++)//This will set every variable in the array to equal 0
-	{
-		appointment[i]=0;
-	}
-	while(1)//Never-ending loop until exiting manually
-	{
-		printf("To schedule an appointment enter(1); To cancel an appointment enter(2); To list all appointment hours enter (3); To exit enter any other number\n");
-		scanf("%d",&action);
-		if (action==1)//Confirming user enters option to make an appointment.
-		{
-			for (i=0;i<6;i++)//Sets the loop for assigning an appointment
-			{
-				if (counter==6)//outputs that all appointments are full since there are 6 appointments scheduled
-				{
-					printf("There are currently no available appointments\n");
-					break;
-				}
-				if (appointment[i]==0)//searches for available position in array where appointment can be set and adds appointment to size of counter
-				{
-					appointment[i]=1;
-					counter++;
-					printf("You will be appointment number:%d at %d p.m.\n",i+1,i+1);
-					break;
-				}
-			}
-		}
-		else if (action==2)//sets condition for when 2 is entered in order to cancel an appointment
-		{
-			printf("please enter the appointment number that you want to cancel.\n");
-			scanf("%d",&appnum);
-			if (appointment[appnum-1]==1)//check to see if appointment number is valid and will cancel the appointment and set the element in that position back to 0	
-			{
-				appointment[appnum-1]=0;
-				printf("Your appointment has been cancelled.\n");
-				counter--;
-			}
-			else if (appointment[appnum-1]==0)//checks condition of appointment number and if there is no appointment scheduled it will read back no appointment
-			{
-				printf("There is no scheduled appointment for this number.\n");
-			}
-		}
-		else if (action==3)//sets condition for when 3 is entered in order to see all scheduled appointments
-		{
-			for (i=0;i<6;i++)//loop to output all elements in array and see which appointments are taken and open
-			{
-				printf("scheduled appointments at %d p.m. :%d \n",i+1,appointment[i]);
-			}
-		}
-		else//condition for when any other number is entered in order to exit never-ending loop
-		{
-			return 0;
-		}
-	}
+        int action;
+        for (i=0;i<6;i++)//Empty out each string in array
+        {
+                names[i][0]='\0';
+        }
+        while(1)
+        {
+                printf("To schedule an appointment enter (1), To cancel an appointment enter (2), To list all appointments enter (3), If you have an emergency and need to schedule an appointment right away enter (4), To check how many times a letter reoccurs in an appointment's name enter (5), To exit enter (6)\n");
+
+                scanf("%d",&action);
+                switch (action)
+                {
+                        case 1:
+                                make();
+                                break;
+                        case 2:
+                                delete();
+                                break;
+                        case 3:
+                                view();
+                                break;
+			case 4:
+				emergency();
+				break;
+			case 5:
+				check();
+				break;
+                        default:
+                                return 0;//To exit              
+                }
+        }
 }
+
+
